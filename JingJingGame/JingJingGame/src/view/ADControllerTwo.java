b1c9a7b1b6556f3dbca6541ab5f3d53714c86d02
ximxain <87000806+ximxain@users.noreply.github.com>
2021-11-17 11:31:15 +0900

    b

---
+++ JingJingGame/JingJingGame/src/view/ADControllerTwo.java
@@ -0,0 +1,47 @@
+package view;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class ADControllerTwo implements Initializable{
+	@FXML
+	private MediaView ads2;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		//Instantiating Media class  
+        Media media = new Media(getClass().getResource("/resource/coding.mp4").toString());  
+          
+        //Instantiating MediaPlayer class   
+        MediaPlayer mediaPlayer = new MediaPlayer(media);  
+          
+        ads2.setMediaPlayer(mediaPlayer); 
+        
+        //by setting this property to true, the Video will be played   
+        
+        
+        
+        mediaPlayer.setOnReady(new Runnable() {
+
+			// 화면이 동영상이 실행 되는 쓰레드 사용
+			@Override
+			public void run() {
+
+				// 시작시 플레이 버튼만 활성화
+				// 나머지는 비활성화
+				mediaPlayer.setAutoPlay(true);  
+
+			}
+		});
+	}
+	
+}
