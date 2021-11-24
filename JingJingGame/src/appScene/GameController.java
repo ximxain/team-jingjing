f7916297667873f5038bf480a9783fa64d24b6c3
seoyeon12345 <seoyeonchoe003@gmail.com>
2021-11-24 12:17:31 +0900

    Revert "s"
    This reverts commit 7561147077417675a642ba2d1bd8f87f01b01956.

--- JingJingGame/src/appScene/GameController.java
+++ JingJingGame/src/appScene/GameController.java
@@ -1,15 +1,12 @@
 package appScene;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -115,7 +112,5 @@ public class GameController extends Login {
 			e.printStackTrace();
 		}
 	}
-
-
 
 }
