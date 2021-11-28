cb49a55cb29e0ef8c6d6e01f030360b6e90607f3
neneong <63086504+neneong@users.noreply.github.com>
2021-11-28 20:25:51 +0900

    f

--- JingJingGame/src/item/itemController.java
+++ JingJingGame/src/item/itemController.java
@@ -8,24 +8,39 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import login.Login;
 import util.AppUtil;
 import util.JDBCUtil;
 
-public class itemController {
+public class itemController extends Login{
 	private ListView<String> lists;
 	private JDBCUtil db;
+	public String food
 	
 	@FXML
 	public void initialize() {
-		lists.getItems().add("ssss");
+		
 		Connection con = db.getConnection();
 		Statement stmt = null;
 		ResultSet rs = null;
 		db = new JDBCUtil();
-		String sql = "SELECT * FROM `jingjing_users` WHERE userId = " + "'" + user + "'";
+		String sql = "SELECT * FROM `food` WHERE userId = " + "'" + user + "'";
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				presentLevel = rs.getInt("food");
+			}
+		}catch(Exception E) {
+			
+		}
 	}
 	
 	public void get() {
 		int idx = lists.getSelectionModel().getSelectedIndex();
+	}
+	
+	public void set(String res) {
+		lists.getItems().add(res);
 	}
 }
