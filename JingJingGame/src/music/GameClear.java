ccd6aec9558df523cf49d126ce9cdfc4bb10468d
miku3984 <ys2110120@y-y.hs.kr>
2021-11-28 20:39:31 +0900

    update

--- JingJingGame/src/music/GameClear.java
+++ JingJingGame/src/music/GameClear.java
@@ -13,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import login.Login;
+import util.AppUtil;
 
 public class GameClear extends Login implements Initializable{
 	@FXML
@@ -53,6 +54,7 @@ public class GameClear extends Login implements Initializable{
 
 			// 30%에 걸리면 호출
 			hungry = 1;
+			AppUtil.alert("당신은 배고파졌습니다! 배고픔 상태일 때는 30% 점수가 삭감됩니다. 상점에서 해결할 수 있습니다.", "");
 		}
 	}
 }
